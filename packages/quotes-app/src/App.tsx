858e57ea8b983693f6314cd343e0a34a778208f4
Rishabh Goswami <rgoswami@csod.com>
2021-11-06 11:43:17 +0530

    Change react-ui-elements to react-copmonents Add quotes app project

--- packages/quotes-app/src/App.tsx
+++ packages/quotes-app/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
 		<div className='App'>
 			<header className='App-header'>
 				<img src={logo} className='App-logo' alt='logo' />
-				<Button label='Click here' onClick={() => alert('hello')}></Button>
+				<Button label='Quotes app' onClick={() => alert('quotes app')}></Button>
 			</header>
 		</div>
 	);

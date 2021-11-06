774a27758acbab755ee59103b3de4b662aa3aa00
Rishabh Goswami <rishabh7g@gmail.com>
2021-11-06 16:34:10 +0530

    Add storybook for react-components

--- packages/react-components/src/components/Button/Button.tsx
+++ packages/react-components/src/components/Button/Button.tsx
@@ -1,12 +1,31 @@
-import React, { MouseEventHandler } from 'react';
-
-type ButtonProps = {
+import './Button.scss';
+interface ButtonProps {
 	label: string;
-	onClick: MouseEventHandler<HTMLButtonElement>;
-};
+	primary?: boolean;
+	size?: 'small' | 'medium' | 'large';
+	style?: object;
+	fontColor?: string;
+	onClick?: () => void;
+}
 
-const Button = ({ label, onClick }: ButtonProps) => (
-	<button onClick={onClick}>{label}</button>
-);
+const Button = ({
+	label,
+	primary = false,
+	size = 'medium',
+	style,
+	fontColor,
+	...props
+}: ButtonProps) => {
+	const mode = primary ? 'button-mode--primary' : 'button-mode--secondary';
+	return (
+		<button
+			type='button'
+			className={['button', `button-size--${size}`, mode].join(' ')}
+			{...style}
+			{...props}>
+			{label}
+		</button>
+	);
+};
 
 export default Button;

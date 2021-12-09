1d026d2b174a0da8eba4a8dad69d5ca033672ec2
Rishabh Goswami <rishabh7g@gmail.com>
2021-12-09 16:13:24 +0530

    Show weather details on home page

--- packages/weather-app/src/App.tsx
+++ packages/weather-app/src/App.tsx
@@ -1,19 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import Weather from 'src/components/weather/Weather';
 import './App.css';
-import { Button } from '@rishabh7g/react-components';
 
 function App() {
+	const [latitude, setLatitude] = useState<number | undefined>();
+	const [longitude, setLongitude] = useState<number | undefined>();
+	const [data, setData] = useState();
+
+	useEffect(() => {
+		navigator.geolocation.getCurrentPosition((position) => {
+			setLatitude(position.coords.latitude);
+			setLongitude(position.coords.longitude);
+
+			const fetchData = async () => {
+				if (latitude && longitude) {
+					const weatherApiUrl = `${process.env.REACT_APP_API_URL}/weather/?lat=${latitude}&lon=${longitude}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`;
+					await fetch(weatherApiUrl)
+						.then((res) => res.json())
+						.then((result) => {
+							setData(result);
+							console.log(result);
+						});
+				}
+			};
+			fetchData();
+		});
+	}, [latitude, longitude]);
+
 	return (
 		<div className='App'>
-			<header className='App-header'>
-				<img src={logo} className='App-logo' alt='logo' />
-				<Button
-					label='Click here'
-					onClick={() => alert('hello')}
-					size='small'
-					primary></Button>
-			</header>
+			{data ? <Weather weatherData={data} /> : <div>data not found</div>}
 		</div>
 	);
 }
